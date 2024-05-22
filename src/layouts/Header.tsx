5bedafe2f788f0df3228342876e955a36f5fbaf9
judahwon <hwondaa@gmail.com>
2024-05-22 23:22:12 +0900

    fix: 파일 경로, 이름 수정

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,16 +1,16 @@
-'use client';
+'use client'
 
-import ThemeSwitch from './ThemeSwitch';
+import ThemeSwitch from '@/src/components/ThemeSwitch'
 
 export default function Header() {
   return (
-    <div
-      className='fixed top-0 left-0 w-full p-2 shadow-md z-10'
-    >
+    <div className='fixed top-0 left-0 w-full p-2 shadow-md z-10'>
       <div className='flex items-center'>
-        <a href='/' className='text-xl font-bold'>훤다log</a>
+        <a href='/' className='text-xl font-bold'>
+          훤다log
+        </a>
         <ThemeSwitch />
       </div>
     </div>
-  );
+  )
 }
--- src/layouts/Theme.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ThemeProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
-
-export default function Theme({ children, ...props }: ThemeProviderProps) {
-  return <ThemeProvider attribute='class' defaultTheme='system' {...props}>{children}</ThemeProvider>;
-}
