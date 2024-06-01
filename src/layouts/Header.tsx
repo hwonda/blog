2b1bade5306121d6c410f1596039135abbfb5834
judahwon <hwondaa@gmail.com>
2024-06-01 12:38:38 +0900

    style: header, postlist layout 스타일 수정

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,16 +1,16 @@
-'use client'
+'use client';
 
-import ThemeSwitch from '@/src/components/ThemeSwitch'
+import ThemeSwitch from '@/src/components/ThemeSwitch';
 
 export default function Header() {
   return (
-    <div className='fixed top-0 left-0 w-full p-2 shadow-md z-10'>
-      <div className='flex items-center'>
-        <a href='/' className='text-xl font-bold'>
+    <div className='top-0 left-0 z-10 fixed flex justify-center shadow-md p-2 w-full'>
+      <div className='flex justify-between items-center w-full max-w-[1200px]'>
+        <a href='/' className='font-bold text-xl'>
           훤다log
         </a>
         <ThemeSwitch />
       </div>
     </div>
-  )
+  );
 }
