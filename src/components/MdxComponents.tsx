c25c906adb8345fb9e16b7368141e1dd0ea82c79
judahwon <hwondaa@gmail.com>
2024-06-19 16:09:16 +0900

    fix: MDXcomponents type error fix

--- src/components/MdxComponents.tsx
+++ src/components/MdxComponents.tsx
@@ -2,7 +2,7 @@ import CodeBlock from './CodeBlock';
 import { MDXComponents } from 'mdx/types';
 
 const MdxComponents: MDXComponents = {
-  pre: CodeBlock,
+  pre: CodeBlock as React.ComponentType,
 };
 
 export default MdxComponents;
