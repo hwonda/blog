b79544af9096ce3b8d65bccaa2bf0eb0a21b472e
judahwon <hwondaa@gmail.com>
2024-06-27 00:34:11 +0900

    feat: generate sitemap.xml and robots.txt
    - 동적으로 sitemap과 robots 생성

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,14 @@
-export const blogName = 'Hwonda Blog';
-export const baseDomain = 'https://www.hwonda.com';
-export const blogDescription = '드디어 블로그 개설?';
-export const blogThumbnailURL = '/public/thumbnail.jpg';
+export const blogMetadata = {
+  name: 'Hwonda Blog',
+  description: '드디어 블로그 개설?',
+  url: 'https://localhost:3000',
+  thumbnailURL: 'https://www.hwonda.com/images/thumbnail.png',
+  author: {
+    name: 'Hwonda',
+    contacts: {
+      email: 'hwondaa@gmail.com',
+      github: 'hwonda',
+      linkedin: 'hwonda',
+    }
+  }
+};
