9a0c21b68d59329ccb8674e4ceb2aa583d753a1e
judahwon <hwondaa@gmail.com>
2024-08-02 14:47:00 +0900

    style: postcard 브라우저 크기에 따라 스타일 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,7 +12,7 @@ const PostCard = ({ post }: PostCardProps) => {
   return (
     <Link href={post.url}>
       <li className='shadow-md hover:shadow-xl border rounded-md h-full transition dark:border-black dark:hover:border-white flex sm:block'>
-        <div className='relative m-2 sm:w-[calc(100% - 4rem)] aspect-video'>
+        <div className='relative sm:m-2 opacity-0 sm:opacity-100 sm:text-lg sm:w-[calc(100% - 4rem)] aspect-video'>
           <Image
             src={post.thumbnail}
             alt={post.title}
@@ -22,7 +22,7 @@ const PostCard = ({ post }: PostCardProps) => {
             className='rounded-md'
           />
         </div>
-        <div className='flex flex-col px-2 pb-2'>
+        <div className='flex flex-col px-2 pb-2 m-2 w-[100%] sm:m-0 sm:w-[auto]'>
           <div className='flex justify-between p-1'>
             <div className='w-auto px-3 rounded-full border border-orange-500 dark:border-orange-400'>
               <p className='impact-color'>{post.categoryPublicName}</p>
