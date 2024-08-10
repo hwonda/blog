3552ac726106721bb7aec4061008f62d39f89271
judahwon <hwondaa@gmail.com>
2024-08-10 19:33:21 +0900

    fix: 작은 화면에서PostCard image hidden

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,7 +12,7 @@ const PostCard = ({ post }: PostCardProps) => {
   return (
     <Link href={post.url}>
       <li className='shadow-md hover:shadow-xl border rounded-md h-full transition dark:border-black dark:hover:border-white flex sm:block'>
-        <div className='relative sm:m-2 opacity-0 sm:opacity-100 sm:text-lg sm:w-[calc(100% - 4rem)] aspect-video'>
+        <div className='relative sm:m-2 hidden sm:block sm:text-lg sm:w-[calc(100% - 4rem)] aspect-video'>
           <Image
             src={post.thumbnail}
             alt={post.title}
