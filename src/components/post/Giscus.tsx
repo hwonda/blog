3d8e38e1b76cb5c755a9f8aaf94311d30cb70c62
judahwon <hwondaa@gmail.com>
2024-08-21 23:45:33 +0900

    docs: reaction 유도글 작성

--- src/components/post/Giscus.tsx
+++ src/components/post/Giscus.tsx
@@ -6,11 +6,16 @@ import { useTheme } from 'next-themes';
 export default function Giscus() {
   const refGiscus = useRef<HTMLDivElement | null>(null);
   const { theme } = useTheme();
-  
+
   // dark mode 전환 시 giscus theme 변경
   useEffect(() => {
-    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
-    iframe?.contentWindow?.postMessage({ giscus: { setConfig: { theme } } }, 'https://giscus.app');
+    const iframe = document.querySelector<HTMLIFrameElement>(
+      'iframe.giscus-frame'
+    );
+    iframe?.contentWindow?.postMessage(
+      { giscus: { setConfig: { theme } } },
+      'https://giscus.app'
+    );
   }, [theme]);
 
   useEffect(() => {
@@ -31,21 +36,25 @@ export default function Giscus() {
       'data-lang': 'en',
       'data-loading': 'lazy',
     };
-    
+
     Object.entries(attributes).forEach(([key, value]) => {
       scriptElem.setAttribute(key, value);
     });
     scriptElem.src = 'https://giscus.app/client.js';
     scriptElem.async = true;
     scriptElem.crossOrigin = 'anonymous';
-    
-    refGiscus.current.appendChild(scriptElem);
 
+    refGiscus.current.appendChild(scriptElem);
   }, [theme]);
 
   return (
-  <div className='mt-10 pt-10 border-t dark:border-gray-400'>
-    <div ref={refGiscus}></div>
-  </div>
+    <div className='mt-10 pt-10 border-t dark:border-gray-400'>
+      <div className='flex justify-center'>
+        <p>-이 글이 여러분에게 도움이 되었으면 </p>
+        <p className='impact-color mx-1'>reactions</p>
+        <p>를 눌러주세요. 감사합니다.-</p>
+      </div>
+      <div ref={refGiscus}></div>
+    </div>
   );
 }
