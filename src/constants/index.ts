18a2692067b6b026c2b261f7d00271d9c4862bbb
judahwon <hwondaa@gmail.com>
2024-11-03 21:48:13 +0900

    chore: favicon 변경

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const blogMetadata = {
-  name: 'hwonda / blog',
+  name: '훤다 블로그',
   description: '드디어 블로그 개설?',
   url: 'https://hwonda.com',
   thumbnailURL: 'https://www.hwonda.com/images/thumbnail.png',
