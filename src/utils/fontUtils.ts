a764d6a1de18e74fb529c9c02188681d82513c1f
judahwon <hwondaa@gmail.com>
2024-11-05 21:13:20 +0900

    fix: 대소문자 오류 해결

--- src/utils/fontUtils.ts
+++ src/utils/fontUtils.ts
@@ -4,47 +4,47 @@ export const fontPretendard = pretendard({
   variable: '--font-pretendard',
   src: [
     {
-      path: '../app/fonts/pretendard/pretendard-Black.woff2',
+      path: '../app/fonts/pretendard/Pretendard-Black.woff2',
       weight: '900',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-ExtraBold.woff2',
+      path: '../app/fonts/pretendard/Pretendard-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-Bold.woff2',
+      path: '../app/fonts/pretendard/Pretendard-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-SemiBold.woff2',
+      path: '../app/fonts/pretendard/Pretendard-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-Medium.woff2',
+      path: '../app/fonts/pretendard/Pretendard-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-Regular.woff2',
+      path: '../app/fonts/pretendard/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-Thin.woff2',
+      path: '../app/fonts/pretendard/Pretendard-Thin.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-Light.woff2',
+      path: '../app/fonts/pretendard/Pretendard-Light.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../app/fonts/pretendard/pretendard-ExtraLight.woff2',
+      path: '../app/fonts/pretendard/Pretendard-ExtraLight.woff2',
       weight: '100',
       style: 'normal',
     },
