8519a94cbd4d6c5b202ae422f9210bb8ea21ffcc
judahwon <hwondaa@gmail.com>
2025-09-10 00:30:40 +0900

    test: add blog beforefiles

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,18 @@ import { withMicrofrontends } from '@vercel/microfrontends/next/config';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      { source: '/blog/:path*', destination: '/:path*' },
-      { source: '/blog/images/:path*', destination: '/images/:path*' }
-    ]
+    return {
+      beforeFiles: [
+        { source: '/blog/_next/:path*',   destination: '/_next/:path*' },
+        { source: '/blog/images/:path*',  destination: '/images/:path*' },
+        { source: '/blog/posts/:path*',   destination: '/posts/:path*'  },
+        { source: '/blog/rss/:path*',  destination: '/rss/:path*' },
+      ],
+      // 나머지 모든 페이지/데이터
+      fallback: [
+        { source: '/blog/:path*', destination: '/:path*' },
+      ],
+    }
   },
 };
 
